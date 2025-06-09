10e106f9c8daae65df03bca3543cf970331a59dc
aman pandey <amanpandey2707@gmail.com>
2025-06-09 22:14:50 +0530

    google login initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 function App() {
   return (
-    <GoogleOAuthProvider clientId="your_google_id">
+    <GoogleOAuthProvider clientId={clientId}>
       <Home />
     </GoogleOAuthProvider>
   );
