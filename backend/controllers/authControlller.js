4a3c249af585782d5e3e8a95d7bfb438445a9263
aman pandey <amanpandey2707@gmail.com>
2025-08-14 00:22:34 +0530

    protected routing

--- backend/controllers/authControlller.js
+++ backend/controllers/authControlller.js
@@ -7,6 +7,30 @@ import { signToken } from "../utils/signToken.js";
 
 const SECRET_KEY = process.env.JWT_SECRET || "super_secret_key";
 
+export const protect = (req, res, next) => {
+  let token;
+
+  // Check for token in Authorization header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Attach decoded user info to request
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
 export const signup = CatchAsync(async (req, res, next) => {
   const { username, email, password, confirmPassword } = req.body;
 
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,9 +3,10 @@ import {
   getAllStocks,
   getStockBySymbol,
 } from "../controllers/stockController.js";
+import { protect } from "../controllers/authControlller.js";
 const router = express.Router();
 
 router.get("/", getAllStocks);
-router.get("/:symbol", getStockBySymbol);
+router.get("/:symbol", protect, getStockBySymbol);
 
 export default router;
